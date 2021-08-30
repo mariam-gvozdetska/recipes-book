2d480dbebcc20c95ebc04845c13af83fbb5532b8
Alexander-pixel <tkachenkosasha17@gmail.com>
2021-08-30 17:13:45 +0300

    Added CMake to the server part of the project.

---
+++ RecipesBook/RecipesBookServer/RecipesBookServer/build/CMakeFiles/Users/aleksandrtkacenko/Desktop/Работа/Eleks/RecipesBook/RecipesBook/RecipesBookServer/RecipesBookServer/Server.cpp.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for /Users/aleksandrtkacenko/Desktop/Работа/Eleks/RecipesBook/RecipesBook/RecipesBookServer/RecipesBookServer/Server.cpp.
