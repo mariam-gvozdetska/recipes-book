b542e83c68770e0e0a5190e3a91adcab34a16cee
Alexander <58190800+Alexander-pixel@users.noreply.github.com>
2021-09-20 22:46:23 +0300

    Update RecipesBook.hpp

--- RecipesBook/RecipesBookClient/RecipesBookClient/include/RecipesBook.hpp
+++ RecipesBook/RecipesBookClient/RecipesBookClient/include/RecipesBook.hpp
@@ -1,25 +1,25 @@
 #pragma once
 
-#include "Dish.hpp"
+#include "Dish.h"
 
 class RecipesBook
 {
     std::vector<Dish> _recipes;
     std::string _title;
-    
+
 public:
     RecipesBook();
-    RecipesBook(std::vector<Dish> recipes, string title);
-    
+    RecipesBook(std::vector<Dish> recipes, std::string title);
+
     void AddDish(Dish dish);
     void DeleteDish(Dish dish);
-    
+
     void SetTitle(std::string title);
     std::string GetTitle()const;
-    
+
     void Print()const;
-    
-    vector<Dish> GetByCuisine(Cuisine cuisine);
-    vector<Dish> GetByType(DishType dishType);
-    vector<Dish> GetByProduct(Product product);
+
+    std::vector<Dish> GetByCuisine(Cuisine cuisine);
+    std::vector<Dish> GetByType(DishType dishType);
+    std::vector<Dish> GetByProduct(Product& product);
 };
